80687c37b8511e780264f857d0749663af821615
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-14 20:15:15 -0800

    Add font-family from google web fonts

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var greenScreen = new Vue({
 	el: '#greenScreen',
 	data: {
-		valign: 'Middle',
-		message: 'Hello Jawsh!'
+		font: 'monospace',
+		message: 'Hello Jawsh!',
+		valign: 'Middle'
 	},
 	computed: {
-		wrapperClass: function () {
+		fontClass: function () {
+			return {
+				// This will turn into the class monospace, serif, and sansSerif
+				// Depending on the value="" of the selected font dropdown
+				// And maps to the #message.[monospace|serif|sansSerif] css class
+				[`${this.font}`]: true
+			}
+		},
+		valignClass: function () {
 			return {
 				// This will turn into the class valignTop, valignMiddle, valignBottom
 				// Depending on the value="[Top|Middle|Bottom]" of the selected radio
