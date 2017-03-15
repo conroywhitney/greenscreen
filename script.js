e13fe3afd12de7e63e6f20a4d3af241652c5a76a
Conroy Whitney <conroy.whitney@gmail.com>
2017-03-14 20:51:49 -0800

    Convert tabs into spaces (thanks, Atom)

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 var greenScreen = new Vue({
-	el: '#greenScreen',
-	data: {
-		fontFamily: 'monospace',
-		fontSize: '96',
-		message: 'Hello Jawsh!',
-		valign: 'middle'
-	},
-	computed: {
-		messageStyle: function () {
-			var fontFamily = {
-				'monospace': "'VT323', monospace",
-				'sansSerif': "'Open Sans', sans-serif",
-				'serif': "'Droid Serif', serif"
-			}[this.fontFamily]
+  el: '#greenScreen',
+  data: {
+    fontFamily: 'monospace',
+    fontSize: '96',
+    message: 'Hello Jawsh!',
+    valign: 'middle'
+  },
+  computed: {
+    messageStyle: function () {
+      var fontFamily = {
+        'monospace': "'VT323', monospace",
+        'sansSerif': "'Open Sans', sans-serif",
+        'serif': "'Droid Serif', serif"
+      }[this.fontFamily]
 
-			return {
-				fontFamily,
-				fontSize: `${this.fontSize}px`
-			}
-		},
-		wrapperStyle: function () {
-			var justifyContent = {
-				top: 'flex-start',
-				middle: 'center',
-				bottom: 'flex-end'
-			}[this.valign]
+      return {
+        fontFamily,
+        fontSize: `${this.fontSize}px`
+      }
+    },
+    wrapperStyle: function () {
+      var justifyContent = {
+        top: 'flex-start',
+        middle: 'center',
+        bottom: 'flex-end'
+      }[this.valign]
 
-			return {
-				justifyContent
-			}
-		}
-	}
+      return {
+        justifyContent
+      }
+    }
+  }
 })
